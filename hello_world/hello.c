7424c6065f2197121bb0564b88fd13b8d7535eb7
sharmavipul92 <vipuls1234@gmail.com>
2018-04-06 04:11:33 +0800

    with added input and default parameter

--- hello_world/hello.c
+++ hello_world/hello.c
@@ -2,15 +2,19 @@
 #include <linux/init.h>
 #include <linux/module.h>
 MODULE_LICENSE("GPL");
+
+static char *who = "Vipul";
+module_param(who, charp, S_IRUGO);
+
 static int hello_init(void)
 {
-	printk(KERN_ALERT "A0178385M: Hello, world\n");
+	printk(KERN_ALERT "A0178385M: Hello, %s\n", who);
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "A0178385M: Goodbye, cruel world\n");
+	printk(KERN_ALERT "A0178385M: Goodbye, %s\n", who);
 }
 
 
